67604c548a3339a0442254a52f0abcdeeca9c793
wcw <1341741114@qq.com>
2017-03-02 09:12:14 +0800

    ButtonSC and Light

---
+++ KillGame/Assets/Button.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour
+{
+    public GameObject PlayerUI;
+    public GameObject CreatButton;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        
+        UIEventListener.Get(CreatButton).onClick = CreatButtonFunc;
+    }
+    void CreatButtonFunc(GameObject ob)
+    {
+        GameObject temp = Instantiate(PlayerUI) as GameObject;
+        temp.transform.parent = gameObject.transform;
+        temp.transform.localPosition = Vector3.zero;
+        temp.transform.localScale = Vector3.one;
+    }
+}
